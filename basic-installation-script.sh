a0d4d86905a5063cf01041353509543c3167b144
Adrian Blanco <adrian.blanco@mobgen.com>
2019-10-09 15:37:29 +0200

    Added basic software

---
+++ basic-installation-script.sh
@@ -0,0 +1,31 @@
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Code
+brew cask iterm2
+brew cask visual-studio-code
+brew cask google-chrome
+brew cask docker
+brew cask sourcetree
+brew cask postman
+brew cask sequel-pro
+brew cask robo-3t
+
+# Storage
+brew cask dropbox
+brew cask keka
+
+# Communication
+brew cask slack
+brew cask telegram
+brew cask whatsapp
+brew cask zoomus
+
+# Entertainment
+brew cask spotify
+brew cask helium
+brew cask epic-games
+brew cask steam
+
+# 3D
+brew cask meshmixer
